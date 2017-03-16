f81eb7c4fd2fd669f1ccff2999bfa98819905838
Lourenzo Ferreira <lourenzo@gmail.com>
2017-03-16 09:10:35 -0300

    Second usage experiment using fetch
    Simple functions, Promises and node-fetch

---
+++ api-model/api-debug-fetch.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const debug = require('debug')('node-prestodb:index.js');
+const fetch = require('node-fetch');
+
+// @TODO: move to configs
+const prestoURL = 'localhost:8080';
+const prestoHeaders = { 'X-Presto-User': 'presto' };
+
+function loopNextUri(uri) {
+  debug(`loopNextUri('${uri}')`);
+  return new Promise((resolve, reject) => {
+    fetch(uri, { headers: prestoHeaders })
+      .then((res) => res.json())
+      .then((body) => {
+        debug(`JSON body props: ${Object.keys(body)}`);
+
+        // has columns
+
+        // has data
+        if (body.data) {
+          debug('emit data');
+          //, body.data.toString());
+        }
+        
+
+        // has nextUri
+        if (body.nextUri) {
+          setTimeout(() => {
+            loopNextUri(body.nextUri);
+          }, 200);
+        } else {
+          resolve(body);
+        }
+        
+      })
+      .catch(reject);
+  });
+}
+
+function sendStatement(query) {
+  return new Promise((resolve, reject) => {
+    fetch(`${prestoURL}/v1/statement`, {
+      method: 'POST',
+      body: query,
+      headers: prestoHeaders
+    }).then((res) => res.json())
+      .then((body) => {
+        debug('JSON body props: ', Object.keys(body));
+        debug(`emit stats {${Object.keys(body.stats)}}`);
+        resolve(loopNextUri(body.nextUri));
+      })
+      .catch(reject);
+  });
+}
+
+let query = 'SELECT * FROM hive.vivo.linhas_teste LIMIT 10';
+sendStatement(query).then(() => {
+  console.log('Good');
+  console.log(arguments);
+}).catch((e) => {
+  console.error('Bad');
+  console.error(e);
+});
+
+
+

1f5afed11a57303519128f9fe45eb9c006d27aee
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-08-26 18:35:58 -0400

    Fix tests

--- test/01.instantiate.js
+++ test/01.instantiate.js
@@ -16,29 +16,34 @@ describe('Create a Client instance', () => {
   let clientInstance;
 
   it('Creates a valid Client Object', () => {
-    clientInstance = new Client('http://localhost:8080');
+    clientInstance = new Client({
+      url: 'http://localhost:8080',
+      user: 'presto',
+      catalog: 'catalog',
+      schema: 'schema',
+      nextUriTimeout: 200
+    });
     expect(clientInstance).to.be.a('object');
 
     expect(clientInstance).to.be.an.instanceOf(Client);
 
-    expect(clientInstance).to.haveOwnProperty('serverURL');
-    expect(clientInstance).to.haveOwnProperty('catalog');
+    expect(clientInstance).to.haveOwnProperty('config');
 
-    expect(clientInstance).to.have.property('runStatement')
+    expect(clientInstance).to.have.property('sendStatement')
       .which.is.a('function');
   });
 
-  it('Returns a valid Statement on runStatement', () => {
-    let stmt = clientInstance.runStatement('SELECT \'a\' as a');
-    expect(stmt).to.be.a('object');
-
-    expect(stmt).to.be.an.instanceOf(Statement);
-
-    expect(stmt).to.have.property('on')
-      .which.is.a('function');
-    expect(stmt).to.have.property('emit');
-
-
+  it('Returns successful promise for sendStatement', function () {
+    const stmt = clientInstance.sendStatement('SELECT \'a\' as a');
+    expect(stmt).to.be.a('promise');
+    return stmt
+      .then(result => {
+        expect(result).to.be.a('object');
+        expect(result.data).to.have.lengthOf(1);
+        expect(result.data[0][0]).to.equal('a');
+        expect(result.columns).to.have.lengthOf(1);
+        expect(result.columns[0].name).to.equal('a');
+      });
   });
 
 });
